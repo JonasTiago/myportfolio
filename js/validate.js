6c7faa72be98e6d4295db91e3f735c78c99c4b22
jonasTiago <jonastiago95@gmail.com>
2023-02-22 20:49:48 -0300

    refactor: clear code

---
+++ js/validate.js
@@ -0,0 +1,58 @@
+const menu = document.querySelector('#menu');
+const arrow = document.querySelector(".arrow");
+const test = document.querySelectorAll("[data-repo]")
+console.log(test)
+
+document.addEventListener('scroll', (event) => {
+  const scrollPosition = event.target.scrollingElement.scrollTop;
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+  // console.log(arrow)
+  if (scrollPosition > 10) {
+    if (!menu.classList.contains('scroll')) {
+      menu.classList.add('scroll');
+      arrow.classList.remove("top");
+    }
+  } else {
+    if (menu.classList.contains('scroll')) {
+      menu.classList.remove('scroll');
+      arrow.classList.add("top")
+    }
+  }
+
+  Array.from(document.querySelectorAll('[data-anime]')).forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add('animation');
+    } else {
+      element.classList.remove('animation');
+    }
+  });
+});
+
+Array.from(document.querySelectorAll('[data-input]')).forEach((e) => {
+  e.addEventListener('focus', (event) => {
+    e.parentNode.classList.add('form-focus');
+    e.parentNode.classList.remove('form-check');
+  });
+
+  e.addEventListener('blur', (event) => {
+    if (event.target.value.length !== 0) {
+      e.parentNode.classList.add('form-check');
+    } else {
+      e.parentNode.classList.remove('form-focus');
+    }
+  });
+});
+
+// Array.from(document.querySelectorAll("[data-repo]")).forEach(e=>{
+//   console.log()
+//   e.addEventListener("mouseover", element =>{
+//     element.target.children[0].classList.remove("invisible");
+//   })
+//   e.addEventListener("mouseleave", element =>{
+//     // element.target.classList.remove("invisible");
+//     element.target.children[0].classList.add("invisible")
+
+//   })
+// })
+
+
